38d42dd217ef02e309d33384691d4cd844d5bf20
alancaras <alancaras@gmail.com>
2019-04-12 10:25:14 +1000

    initial commit

---
+++ Javascript/script.js
@@ -0,0 +1,13 @@
+function myFunction() {
+  const animals = ["Grizzly Bear", "Sloth", "Sea Lion"];
+  for (let i = 0; i < animals.length; i++) {
+    console.log(i);
+    console.log(animals[i]);
+  }
+  return 1;
+}
+
+function addTwo(x, y) {
+  console.log(x + y);
+  alert("hi");
+}
